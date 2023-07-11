51509734df425e57d273e38a9533a9b7b6e79946
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-07-11 17:31:20 -0400

    Added loading animation
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- gui.py
+++ gui.py
@@ -6,6 +6,12 @@ from layout import create_layout
 from plot_source_1_band import plot_source
 from init import find_datapoints
 
+def find_datapoints_threaded(dir):
+    try:
+        find_datapoints(dir)
+    except:
+        pass
+
 def run_gui():
     
     sg.theme("DarkGrey5")
@@ -14,6 +20,7 @@ def run_gui():
     source_dict = {}
     sources = []
     dir = ""
+    new_dir = ""
     scrollable = False
 
     stations = list(set(pd.read_csv('data/derived/stations.csv')['name']))
@@ -22,10 +29,11 @@ def run_gui():
     main_window = sg.Window('Quasar Viewer', layout,
                             margins=[20, 20], resizable=True, finalize=True, icon="images/favicon.ico")
 
-
+    show_gif = False
     # Event loop for the GUI
     while True:
-        window, event, values = sg.read_all_windows()
+        # window, event, values = sg.read_all_windows()
+        event, values = main_window.Read(timeout=25)
 
         if event == "source_list":
             if values["source_list"] == []:
@@ -40,7 +48,7 @@ def run_gui():
                 main_window[f"{box}_1"].update(visible=False)
                 main_window[f"{box}_1"].hide_row()
             
-            scrollable = len(source_dict[values["source_list"]]) > 10
+            scrollable = len(source_dict[values["source_list"]]) > 8
 
             for box in source_dict[values["source_list"]]:
                 if scrollable:
@@ -57,6 +65,7 @@ def run_gui():
             
             main_window["check_box_col"].contents_changed()
             main_window["check_box_col_scroll"].contents_changed()
+            main_window["stations_col"].contents_changed()
             main_window.refresh()
 
         if event == sg.WIN_CLOSED or event == "cancel" or event == "Exit":
@@ -84,36 +93,42 @@ def run_gui():
             plot_source(source, '', dir, ignored_stations, band)
 
         if event == "Open folder":
-            old_dir = dir
             new_dir = askdirectory(initialdir="data/sessions")
-            if new_dir != old_dir and new_dir:
-                try:
-                    find_datapoints(new_dir)
-
-                    for box in stations:
-                        main_window[box].update(visible=False)
-                        main_window[box].hide_row()
-                        main_window[f"{box}_1"].update(visible=False)
-                        main_window[f"{box}_1"].hide_row()
-                    main_window["check_box_col"].contents_changed()
-                    main_window["check_box_col_scroll"].contents_changed()
-
-                    source_dict = find_station_matches()
-                    sources = list(source_dict.keys())
-                    main_window["source_list"].update(values=sources)
-
-                    main_window.set_title(f"Quasar Viewer - {new_dir.split('/')[-1]}")
-                    main_window.refresh()
-                    dir = new_dir
-                except:
-                    sg.Popup("Not a valid vgosDB!")
+            if new_dir != dir and new_dir:
+                show_gif = True
+                sg.PopupAnimated("images/loading.gif", background_color="black", time_between_frames=100)
+                main_window.perform_long_operation(lambda : find_datapoints_threaded(new_dir),"load_done")
 
         if event == "About...":
             # Change title
             sg.Popup("About info")
 
-            
+        if event in stations:
+            main_window[f"{event}_1"].update(value=values[event])
+        if event[:-2] in stations:
+            main_window[event[:-2]].update(value=values[event])
 
+        if event == "load_done":
+            for box in stations:
+                main_window[box].update(visible=False)
+                main_window[box].hide_row()
+                main_window[f"{box}_1"].update(visible=False)
+                main_window[f"{box}_1"].hide_row()
+            main_window["check_box_col"].contents_changed()
+            main_window["check_box_col_scroll"].contents_changed()
+
+            source_dict = find_station_matches()
+            sources = list(source_dict.keys())
+            main_window["source_list"].update(values=sources)
+
+            main_window.set_title(f"Quasar Viewer - {new_dir.split('/')[-1]}")
+            main_window.refresh()
+            dir = new_dir
+            show_gif = False
+            sg.PopupAnimated(None)
+
+        if show_gif:
+            sg.PopupAnimated("images/loading.gif", background_color="black", time_between_frames=50, grab_anywhere=False)
 
 if __name__ == '__main__':
     run_gui()
--- layout.py
+++ layout.py
@@ -3,12 +3,14 @@ import PySimpleGUI as sg
 
 def create_layout(stations):
     stations.sort()
-    check_boxes1 = list(map(lambda s: [sg.Checkbox(s,default=True,visible=False,key=s,expand_x=True)],stations))
-    check_boxes2 = list(map(lambda s: [sg.Checkbox(s,default=True,visible=False,key=f"{s}_1",expand_x=True)],stations))
+    check_boxes1 = list(map(lambda s: [sg.Checkbox(s,default=True,visible=False,key=s,expand_x=True,enable_events=True)],stations))
+    check_boxes2 = list(map(lambda s: [sg.Checkbox(s,default=True,visible=False,key=f"{s}_1",expand_x=True,enable_events=True)],stations))
+
+    stations_col = sg.Column([[sg.Column(check_boxes1, s=(260,260), scrollable=False, key="check_box_col", element_justification="left", expand_x=True, expand_y=True, p=20),
+                               sg.Column(check_boxes2, s=(260,260), scrollable=True, vertical_scroll_only = True, key="check_box_col_scroll", element_justification="left", expand_x=True, expand_y=True, p=20, visible=False)]], key="stations_col", s=(300,300), scrollable=False)
 
     left_col = [[sg.Frame("Source",[[sg.Combo([[]], key="source_list", size=(30,30), enable_events=True, expand_x=True, p=20, readonly=True)]], expand_x=True)],
-                [sg.Frame("Stations",[[sg.Column(check_boxes1, s=(300,300), scrollable=False, key="check_box_col", element_justification="left", expand_x=True, expand_y=True, p=20)],
-                                      [sg.Column(check_boxes2, s=(300,300), scrollable=True, vertical_scroll_only = True, key="check_box_col_scroll", element_justification="left", expand_x=True, expand_y=True, p=20, visible=False)]],expand_x=True)],
+                [sg.Frame("Stations",[[stations_col]],expand_x=True)],
                 [sg.Frame("Band",[[sg.Radio("A","band", key='A_band', default=True),sg.Radio("B","band", key="B_band"),sg.Radio("C","band", key="C_band"),sg.Radio("D","band", key="D_band")]],expand_x=True)],
                 [sg.VPush()],
                 [sg.Push(),sg.Button("Plot",key="plot"),sg.Button("Cancel",key="cancel")]]
