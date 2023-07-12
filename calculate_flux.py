206b5c47198c78d1277146af762b3a7debb3a067
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-07-12 16:55:52 -0400

    Added check to avoid division by 0
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- calculate_flux.py
+++ calculate_flux.py
@@ -31,19 +31,19 @@ def calculate_flux(index, general_data, station_data, band=-1):
 
     A_SNR = general_data.A_SNR.iloc[index]
     A_band_width = general_data.A_bw.iloc[index]*10**6
-    A_flux = A_SNR * sqrt(SEFD1_S * SEFD2_S) / (C * sqrt(2*int_time*A_band_width))
+    A_flux = A_SNR * sqrt(SEFD1_S * SEFD2_S) / (C * sqrt(2*int_time*A_band_width)) if A_band_width != 0 else 0
 
     B_SNR = general_data.B_SNR.iloc[index]
     B_band_width = general_data.B_bw.iloc[index]*10**6
-    B_flux = B_SNR * sqrt(SEFD1_X * SEFD2_X) / (C * sqrt(2*int_time*B_band_width))
+    B_flux = B_SNR * sqrt(SEFD1_X * SEFD2_X) / (C * sqrt(2*int_time*B_band_width)) if B_band_width != 0 else 0
 
     C_SNR = general_data.C_SNR.iloc[index]
     C_band_width = general_data.C_bw.iloc[index]*10**6
-    C_flux = C_SNR * sqrt(SEFD1_X * SEFD2_X) / (C * sqrt(2*int_time*C_band_width))
+    C_flux = C_SNR * sqrt(SEFD1_X * SEFD2_X) / (C * sqrt(2*int_time*C_band_width)) if C_band_width != 0 else 0
 
     D_SNR = general_data.D_SNR.iloc[index]
     D_band_width = general_data.D_bw.iloc[index]*10**6
-    D_flux = D_SNR * sqrt(SEFD1_X * SEFD2_X) / (C * sqrt(2*int_time*D_band_width))
+    D_flux = D_SNR * sqrt(SEFD1_X * SEFD2_X) / (C * sqrt(2*int_time*D_band_width)) if D_band_width != 0 else 0
 
 
     if band == -1:
