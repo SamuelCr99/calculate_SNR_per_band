4127e6ca4a2d57458d7be3c440e98e9381acac3f
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-07-31 17:26:02 -0400

    Started work on new wrapper and readme
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- readme.md
+++ readme.md
@@ -1,1 +1,38 @@
-# 
+# Good name
+
+## Description
+
+Plots the baseline and other related quantities between two VLBI stations. The program was developed at NVI Inc. by Filip Herbertsson and Samuel Collier Ryder during a summer internship in 2023.
+
+![](images/gui_image.png "Image of the GUI")
+
+
+## How to install
+
+To install the program, you need to clone the GitHub repository and install the required libraries with
+
+```bash
+$ git clone https://github.com/SamuelCr99/VLBI_baseline_movement_plotter.git
+$ pip install -r requirements.txt
+```
+
+## How to use
+To run the program, run the main.py file: 
+
+```bash
+$ python3 main.py
+```
+
+### Load session 
+To load a session into the gui, click on the "Load session" button under the "File"
+tab. This session folder should be a VGOS DB.  
+
+### Plotting
+After loading a session, click on one of the sources to plot the u,v coordinates 
+and the flux density. To change which band is plotted simply click on one of the
+four radio buttons under band. 
+
+### Adjusting plot
+
+
+
---
+++ utility/wrappers/stations_config_wrapper.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import os
+
+CONFIG_PATH = "data/derived/config.csv"
+
+class StationsConfigWrapper:
+
+    def __init__(self, *arg):
+        if not arg:
+            create_config()
+            self.df = pd.read_csv(CONFIG_PATH)
+        else:
+            if type(arg) == pd.DataFrame:
+                self.df = arg
+            elif type(arg) == str:
+                self.df = pd.read_csv(arg)
+        self.df_copy = self.df.copy(deep=True)
+    
+    def save(self):
+        self.df.to_csv(CONFIG_PATH, index=False)
+        self.df_copy = self.df.copy(deep=True)
+
+    def restore(self):
+        self.df = self.df_copy.copy(deep=True)
+    
+    def delete(self):
+        create_config()
+        self.df = pd.read_csv(CONFIG_PATH)
+        self.df_copy = self.df.copy(deep=True)
+
+    def has_changed(self):
+        return self.df.equals(self.df_copy)
+
+    def get_SEFD(self, station, band):
+        if type(band) == int:
+            band = ["A","B","C","D","S","X"][band]
+        sefd_col = f"{band}_SEFD"
+        return float(self.df.loc[self.df.name == station,sefd_col].iloc[0])
+    
+    def set_SEFD(self, station, band, sefd):
+        if type(band) == int:
+            band = ["A","B","C","D","S","X"][band]
+        sefd_col = f"{band}_SEFD"
+        self.df.loc[self.df.name == station,sefd_col] = max(round(sefd),1)
+
+    def select(self, station):
+        self.df.loc[self.df.name == station,"selected"] = 1
+
+    def deselect(self, station):
+        self.df.loc[self.df.name == station,"selected"] = 0
+    
+    def is_selected(self, station):
+        return self.df.loc[self.df.name == station,"selected"].iloc[0]
+
+    def toggle(self, station):
+        if self.is_selected(station):
+            self.deselect()
+            return False
+        else:
+            self.select()
+            return True
+
+def create_config():
+    """
+    Finds stations and their SEFD
+
+    Parameter:
+    No parameters!
+
+    Returns:
+    No return values!
+    """
+    station_sefds = pd.read_csv(
+        'data/standard/equip.cat', delim_whitespace=True)[['Antenna', 'X_SEFD', 'S_SEFD']]
+
+    station_sefds['A_SEFD'] = station_sefds['S_SEFD']
+    station_sefds['B_SEFD'] = station_sefds['X_SEFD']
+    station_sefds['C_SEFD'] = station_sefds['X_SEFD']
+    station_sefds['D_SEFD'] = station_sefds['X_SEFD']
+
+    station_locations = pd.read_csv(
+        'data/standard/position.cat', delim_whitespace=True)[['Name', 'X', 'Y', 'Z', 'Lat', 'Lon']]
+    joined_df = pd.merge(station_locations, station_sefds,
+                         left_on='Name', right_on='Antenna')
+    joined_df = joined_df.drop(columns=['Antenna','X','Y','Z','Lat','Lon'])
+
+    joined_df = joined_df.rename(
+        columns={'Name': 'name'})
+
+    joined_df["selected"] = [1]*len(joined_df["name"])
+
+    # Write to csv file
+    data_folders = os.listdir('data/')
+    if 'derived' not in data_folders:
+        os.mkdir('data/derived')
+
+    joined_df.to_csv(CONFIG_PATH, index=False)
